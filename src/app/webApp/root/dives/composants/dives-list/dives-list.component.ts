99261364445fd5ca096dc52d1f21bdb13398a955
Marie <marie.clerc@laplateforme.io>
2024-07-10 16:05:45 +0200

    ViewChild, ViewChildren

--- src/app/webApp/root/dives/composants/dives-list/dives-list.component.ts
+++ src/app/webApp/root/dives/composants/dives-list/dives-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, viewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, viewChild, ViewChildren } from '@angular/core';
 import { DivesService } from '../../../../../shared/services/dives.service';
 import { Dives } from '../../../../../shared/class/dives';
 import { Meta, Title } from '@angular/platform-browser';
@@ -15,8 +15,14 @@ export class DivesListComponent implements OnInit, OnDestroy, AfterViewInit, Aft
 
   private subData:Subscription = new Subscription();
 
+  // ViewChild ne marche que sur le 1et element de la colection
   @ViewChild('btnPlus') eltBtn!:ElementRef<HTMLElement>
   // @ViewChild('btnPlus') eltBtn!:ElementRef<HTMLButtonElement>;
+  
+  // utiliser ViewChildren pour tous les elt de la collection
+  @ViewChildren('btnPlus') eltCollectionBtn!:QueryList<ElementRef>
+
+
 
   // 2- const
   // injection de dépendances :
@@ -70,7 +76,15 @@ export class DivesListComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     // est appelé plusieurs fois
     // CHANGE DETECTION mODE
     console.warn('AfterViewChecked : ', this.eltBtn);
-    
+    // this.eltBtn.nativeElement.innerHTML = 'plus d\'info sur la plongée';
+  
+    this.eltCollectionBtn.map(
+      (elt, indice) => {
+        elt.nativeElement.innerHTML = `Plus d'infos sur la plongée ${indice}`;
+        elt.nativeElement.classList.replace('btn-primary', 'btn-warning');
+        elt.nativeElement.className = 'btn btn-success'
+      }
+    )
   }
   // 4- méthodes
 }
