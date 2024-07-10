4afebc2a2ade32efb521171b9721055c8c52d31e
Marie <marie.clerc@laplateforme.io>
2024-07-10 16:35:23 +0200

    ajout du routage dans le menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './webApp/root/accueil/components/home-page/home-page.component';
+import { DivesListComponent } from './webApp/root/dives/composants/dives-list/dives-list.component';
 
 const routes: Routes = [
   { path:'', component: HomePageComponent}, //www.domain.tld
+  { path:'liste-des-plongées', component:DivesListComponent}
 ];
 
 @NgModule({
